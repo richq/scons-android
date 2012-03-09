64f913a535d5aa281ad3750ebadea1e33121dbbd
Richard Quirk <richard.quirk@gmail.com>
2012-03-09 21:40:40 +0100

    Compile as source/target 1.5

--- android.py
+++ android.py
@@ -303,7 +303,7 @@ def AndroidApp(env, name,
         classes = env.Java(target=bin_classes, source=source,
                            JAVABOOTCLASSPATH='$ANDROID_JAR',
                            JAVASOURCEPATH=gen.path,
-                           JAVACFLAGS='-g -encoding ascii'.split(),
+                           JAVACFLAGS='-target 1.5 -source 1.5 -g -encoding ascii'.split(),
                            JAVACLASSPATH=default_cp)
         env.Depends(classes, rfile)
 
